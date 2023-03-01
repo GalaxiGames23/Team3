c32591352e4d3bbb2d1ec975fb2ed9c2fcb5dc53
Mikhael <choura.mikhael@gmail.com>
2023-03-01 22:37:59 +0100

    Wall finish

--- Wall/DestructibleWall.gd
+++ Wall/DestructibleWall.gd
@@ -8,16 +8,22 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Area2D/CollisionShape2D.disabled = false
+	$CollisionShape2D.disabled = false
+	$WallVert3212.visible = true
+	$Particles2D.emitting= false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+#Function to call when the wall will be destroy
 func break_wall():
 	$AnimationPlayer.play("Break_Wall_Anim")
-
+	
+#Function to call when the wall remain untouched
+func not_break_wall():
+	$AnimationPlayer.play("Not_Break_Wall_Anim")
+	
+#When the ball enter in the area, if enough speed, destroy the wall
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Ball") and body.can_break_wall:
 		break_wall()
+	elif  body.is_in_group("Ball") and !body.can_break_wall:
+		not_break_wall()
