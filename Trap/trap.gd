22d16eac2ff5a1c4d7903ef3bfe6b857f3e23727
Theodore G <t.gigault@gmail.com>
2023-03-06 11:21:17 +0100

    nice level

---
+++ Trap/trap.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+var stop : bool = false
+
+func _ready():
+	$CollisionShape2D.position=Vector2(-170,0)
+	$CollisionShape2D2.position=Vector2(200,115)
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player") and !stop:
+		$AnimationPlayer.play("block")
+		stop=true
