411884a579cbc8f5f1f9fdc3de8c1c35ae83b66c
Theodore G <t.gigault@gmail.com>
2023-03-29 20:19:18 +0200

    some fixes

--- Light/light.gd
+++ Light/light.gd
@@ -4,10 +4,10 @@ export(int, 0, 255) var  red = 255
 export(int, 0, 255) var  green = 255
 export(int, 0, 255) var  blue = 255
 export(int, 0, 100) var  a = 30
-export(float, 0, 1) var  energy = 0.5
+export(float, 0, 1) var  intensity = 0.5
 
 func _ready():
-	$Light2D.energy=energy*0.05
+	$Light2D.energy=intensity*0.05
 	$Light2D.color.r=red
 	$Light2D.color.g=green
 	$Light2D.color.b=blue
